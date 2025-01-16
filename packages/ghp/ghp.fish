cea44dbb826ffbfa907e3bb257226fd141d4b36a
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-16 15:08:12 -0500

    ghp: rewrite in fish

---
+++ packages/ghp/ghp.fish
@@ -0,0 +1,27 @@
+#!/usr/bin/env fish
+
+set DIRECTORY /etc/nixos
+
+function cleanup_state
+    rm -rf $TMPDIR
+end
+
+# Currently unstaged changes
+# We do "$()" anywhere this is used, to not split into a list on newlines
+set diff "$(git -C $DIRECTORY diff)"
+
+# Internal dependency from nix package inputs, split patch into hunks within $TMPDIR
+set TMPDIR "$(hip "$diff")"
+
+# Delete TMPDIR on exit, even if user exits early
+trap cleanup_state EXIT
+
+cd $TMPDIR
+set applied_patches (fzf -m --preview-window="top" --preview="cat {} | diff-so-fancy")
+
+cd $DIRECTORY
+for patch in $applied_patches
+    cat "$TMPDIR/$patch" | git apply --cached - # Stage changes
+end
+
+# $TMPDIR is cleaned up here automatically
