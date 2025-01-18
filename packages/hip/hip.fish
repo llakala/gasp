6c73a55d93c1f3f22aef6ceaa0a73ad9f04cc200
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-01-17 19:44:04 -0500

    hip: silence output from splitpatch
    Previously, splitpatch errors would break things. They don't anymore!

--- packages/hip/hip.fish
+++ packages/hip/hip.fish
@@ -6,7 +6,7 @@ set TMPDIR (mktemp -d)
 cd $TMPDIR
 
 echo $ORIGINAL >"ORIGINAL.patch" # Uses `git diff` for unstaged changes
-splitpatch -H "ORIGINAL.patch" # Split up patch into individual hunks
+splitpatch -H "ORIGINAL.patch" >/dev/null # Split up patch into individual hunks
 rm "ORIGINAL.patch" # Don't need it anymore now that the hunks are split up
 
 echo $TMPDIR # So it can be cd'ed into, used, and then destroyed
