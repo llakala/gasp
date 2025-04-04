98d547f8ebf822a47c3ddd5ded589d0baf15cb1a
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-04 00:45:41 -0400

    fmbl: hide input in normal mode
    New FZF feature! (Well, not that new.)

--- packages/fmbl/package.nix
+++ packages/fmbl/package.nix
@@ -12,9 +12,9 @@ llakaLib.writeFishApplication
       "--border --cycle --exact --highlight-line --inline-info --multi --no-separator --reverse --ansi" \
       "--preview-window '75%'" \
       "--bind j:down --bind k:up" \
-      "--bind 'esc:change-header(NORMAL MODE)+rebind(i,j,k)'" \
-      "--bind 'i:change-header(INSERT MODE)+unbind(i,j,k)'" \
-      "--bind 'start:unbind(down,up)+change-header(NORMAL MODE)'"
+      "--bind 'esc:change-header(NORMAL MODE)+hide-input+rebind(i,j,k)'" \
+      "--bind 'i:change-header(INSERT MODE)+show-input+unbind(i,j,k)'" \
+      "--bind 'start:unbind(down,up)+hide-input+change-header(NORMAL MODE)'"
 
     echo (string join " " -- $options_list) # So user can set a variable to the output
   '';
