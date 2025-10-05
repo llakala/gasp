1f15d67a0d635a6c982b67ffe61149813ff27333
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-05 14:42:04 -0400

    packages/splitpatch: add todo comment

--- packages/splitpatch/package.nix
+++ packages/splitpatch/package.nix
@@ -2,12 +2,14 @@
 
 stdenv.mkDerivation {
   pname = "splitpatch";
+
+  # TODO: bump to latest version
   version = "TEST";
 
   src = fetchFromGitHub {
     owner = "jaalto";
     repo = "splitpatch";
-    rev = "256cb7b5beab2234395096617593d63025f443e0"; # Pointing to my own PR
+    rev = "256cb7b5beab2234395096617593d63025f443e0";
     hash = "sha256-Li4vjov+w7S7uxrGYRrRq7+p4LqM9Bhk6HVx4MBFMzI=";
   };
 
