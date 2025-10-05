97985ec2041069c101b579ed2b5cbccd636741fa
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-10-05 14:39:26 -0400

    flake: small readability tweaks

--- flake.nix
+++ flake.nix
@@ -1,22 +1,16 @@
 {
-  inputs = {
-    # If you want to use `follows`, make it follow your own unstable input
-    # for access to new FZF versions
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { self, nixpkgs, ... }:
   let
-    lib = nixpkgs.lib;
-
-    # The "normal" systems. If it ever doesn't work with one of these, or you want me
-    # to add a system, let me know!
-    supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    inherit (nixpkgs) lib;
 
     forAllSystems = function: lib.genAttrs
-      supportedSystems
+      [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ]
       (system: function nixpkgs.legacyPackages.${system});
   in {
+    # If you want a non-flake way of accessing these packages, just use this
+    # file directly - flake is a simpler wrapper around it.
     legacyPackages = forAllSystems (pkgs: import ./packages/default.nix { inherit pkgs; });
 
     devShells = forAllSystems (pkgs: {
